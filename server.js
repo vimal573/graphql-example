c5dfc16fb9048f83bed43badf5611e3eb052b088
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-12-14 12:19:48 +0530

    Designing An E-Commerce Schema

--- server.js
+++ server.js
@@ -1,24 +1,73 @@
 const express = require('express');
 const { buildSchema } = require('graphql');
-const { graphqlHTTP } = require('express-graphql');
+const { createHandler } = require('graphql-http/lib/use/express');
 
 const schema = buildSchema(`
     type Query  {
-        description: String
-        price: Float
+        products: [Products]
+        order: [Order]
     }
-    `);
+
+    type Product {
+        id: ID!
+        description: String!
+        reviews: [Review]
+        price: Float!
+    }
+
+    type Review {
+        rating:Int!
+        comment:String
+    }
+
+    type Order {
+        date: String!
+        subtotal: Float!
+        items: [OrderItem]
+    }
+    
+    type OrderItem {
+        product: Product!
+        quantity: Int!
+    }
+`);
 
 const root = {
-  description: 'Red Shoe',
-  price: 42.12,
+  products: [
+    {
+      id: 'redshoe',
+      description: 'Red Shoe',
+      price: 42.12,
+    },
+    {
+      id: 'bluejean',
+      description: 'Blue Jean',
+      price: 55.55,
+    },
+  ],
+  orders: [
+    {
+      date: '2005-05-05',
+      subtotal: 90.22,
+      items: [
+        {
+          product: {
+            id: 'redshoe',
+            description: 'Old Red Shoe',
+            price: 45.11,
+          },
+          quantity: 2,
+        },
+      ],
+    },
+  ],
 };
 
 const app = express();
 
 app.use(
   '/graphql',
-  graphqlHTTP({
+  createHandler({
     schema: schema,
     rootValue: root,
     graphiql: true,
